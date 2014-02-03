f422288284ad54b07b320736031806e9154560f7
Valentina Manea <valentina.manea.m@gmail.com>
2014-02-04 01:16:32 +0200

    Migrate usbip_bind to libudev.
    Signed-off-by: Valentina Manea <valentina.manea.m@gmail.com>

---
+++ src/sysfs_utils.c
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "sysfs_utils.h"
+#include "usbip_common.h"
+
+int write_sysfs_attribute(const char *attr_path, const char *new_value,
+			  size_t len)
+{
+	int fd;
+	int length;
+
+	if (attr_path == NULL || new_value == NULL || len == 0) {
+		dbg("Invalid values provided for attribute %s.", attr_path);
+		return -1;
+	}
+
+	if ((fd = open(attr_path, O_WRONLY)) < 0) {
+		dbg("Error opening attribute %s.", attr_path);
+		return -1;
+	}
+
+	length = write(fd, new_value, len);
+	if (length < 0) {
+		dbg("Error writing to attribute %s.", attr_path);		
+		close(fd);
+		return -1;
+	}
+
+	close(fd);
+
+	return 0;
+}
---
+++ src/sysfs_utils.h
@@ -0,0 +1,3 @@
+
+int write_sysfs_attribute(const char *attr_path, const char *new_value,
+			  size_t len);
